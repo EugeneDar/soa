c73ac21fe64044d10c2829d0ede94ea43d58ae42
Eugene Darashkevich <evgenedar@mail.ru>
2024-03-10 22:14:17 +0300

    small fixes

--- src/services/users/app/main.py
+++ src/services/users/app/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from util import get_field
 from models.user import User
@@ -23,6 +24,13 @@ def create_user():
     user_details = request.json
     new_user = user_schema.load(user_details)
 
+    password = generate_password_hash(user_details.get('password'))
+    new_user.password = password
+
+    existing_user = User.query.filter_by(username=new_user.username).first()
+    if existing_user is not None:
+        return jsonify(message="User with such username already exists."), 400
+
     db.session.add(new_user)
     db.session.commit()
 
@@ -34,8 +42,8 @@ def login():
     username = get_field(request, 'username')
     password = get_field(request, 'password')
 
-    user = User.query.filter_by(username=username, password=password).first()
-    if user is None:
+    user = User.query.filter_by(username=username).first()
+    if user is None or not check_password_hash(user.password, password):
         return jsonify(message="Invalid credentials"), 401
 
     access_token = create_access_token(identity=username)
