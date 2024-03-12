446fd3f13e1f1d3c85f437c315bf196f6caa9980
Eugene Darashkevich <evgenedar@mail.ru>
2024-03-12 22:40:00 +0300

    all user api implemented

--- src/services/users/app/main.py
+++ src/services/users/app/main.py
@@ -1,19 +1,18 @@
 import os
 
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_cors import CORS
-from werkzeug.security import generate_password_hash  # TODO do not use
 
 from database.database import db
+from util.util import get_field, password_hasher, connect_to_db
+from schemas.user_schema import UserSchema
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
-from util.util import get_field
 from models.user import User
-from schemas.user_schema import UserSchema
 
 CORS(app)
 
@@ -25,14 +24,16 @@ user_schema = UserSchema()
 @app.route("/signup", methods=["POST"])
 def create_user():
     user_details = request.json
-    new_user = user_schema.load(user_details)
 
-    password = generate_password_hash(user_details.get('password'))[:100]  # TODO use hasher for RS cource
-    new_user['password'] = password
+    username = get_field(request, 'username')
+    password = get_field(request, 'password')
 
-    existing_user = User.query.filter_by(username=new_user['username']).first()
+    existing_user = User.query.filter_by(username=username).first()
     if existing_user is not None:
         return jsonify(message="User with such username already exists."), 400
+
+    new_user = user_schema.load(user_details)
+    new_user['password'] = password_hasher(username, password)
 
     new_user = User(**new_user)
     db.session.add(new_user)
@@ -47,20 +48,24 @@ def login():
     password = get_field(request, 'password')
 
     user = User.query.filter_by(username=username).first()
-    if user is None or not generate_password_hash(password)[:100]:
+    if user is None or password_hasher(username, password) != user.password:
         return jsonify(message="Invalid credentials"), 401
 
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
 
 
-@app.route("/users/<id>", methods=["PUT"])
-@jwt_required
-def update_user(id):
+@app.route("/users/<username>", methods=["PUT"])
+@jwt_required()
+def update_user(username):
     user_details = request.json
-    user = User.query.get(id)
+    user = User.query.filter_by(username=username).first()
     if user is None:
         return jsonify(message="User not found"), 404
+
+    current_user = get_jwt_identity()
+    if current_user != username:
+        return jsonify(message="Unauthorized request"), 401
 
     user = user_schema.load(user_details, partial=True)
 
@@ -69,7 +74,5 @@ def update_user(id):
 
 
 if __name__ == "__main__":
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    connect_to_db(db, app)
     app.run(host='0.0.0.0', debug=True)
--- src/services/users/app/util/util.py
+++ src/services/users/app/util/util.py
@@ -1,4 +1,29 @@
+import hashlib
+import time
+from sqlalchemy.exc import OperationalError
 
 
 def get_field(request, field, default=None):
     return request.json.get(field, default)
+
+
+def password_hasher(username, password):
+    m = hashlib.md5()
+    m.update((username + password).encode())
+    return m.hexdigest()
+
+
+def connect_to_db(db, app, retries=10, delay=2):
+    db.init_app(app)
+
+    while retries > 0:
+        try:
+            with app.app_context():
+                db.create_all()
+            return
+        except OperationalError as err:
+            retries -= 1
+            print('Could not connect to DB, retrying in', delay, 'seconds')
+            time.sleep(delay)
+
+    raise Exception('Can not connect to DB')
