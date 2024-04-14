b035463c0f0ab128b4b28aa82efa17e4dd7a46bd
Eugene Darashkevich <evgenedar@mail.ru>
2024-04-14 16:33:45 +0300

    kafka configured

--- src/services/users/app/main.py
+++ src/services/users/app/main.py
@@ -1,9 +1,10 @@
 import os
-
+import json
 import grpc
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_cors import CORS
+from kafka import KafkaProducer
 
 from database.database import db
 from util.util import get_field, password_hasher, connect_to_db, proto_post_to_dict
@@ -11,17 +12,19 @@ from schemas.user_schema import UserSchema
 from api.posts.posts_pb2 import CreatePostRequest, UpdatePostRequest, DeletePostRequest, GetPostByIdRequest, ListPostsRequest
 from api.posts.posts_pb2_grpc import PostServiceStub
 
+# Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
-
 from models.user import User
-
 CORS(app)
-
 jwt = JWTManager(app)
 
+# MySql
 user_schema = UserSchema()
+
+# Kafka
+producer = None  # init in main
 
 
 @app.route("/signup", methods=["POST"])
@@ -170,21 +173,35 @@ def list_posts():
         'limit': limit
     })
 
+
 @app.route("/posts/<post_id>/views", methods=["POST"])
 @jwt_required()
 def add_post_view(post_id):
-    # TODO
+    message = {
+        'post_id': post_id
+    }
+    producer.send('post_views', message)
     return jsonify({'success': True})
 
 
 @app.route("/posts/<post_id>/likes", methods=["POST"])
 @jwt_required()
 def add_post_like(post_id):
-    # TODO
+    message = {
+        'post_id': post_id
+    }
+    producer.send('post_likes', message)
     return jsonify({'success': True})
-
 
 
 if __name__ == "__main__":
     connect_to_db(db, app)
+
+    producer = KafkaProducer(
+        bootstrap_servers=['kafka:29092'],
+        api_version=(0, 11, 5),
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        request_timeout_ms=3000
+    )
+
     app.run(host='0.0.0.0', debug=True)
