b7b01b9603ab4bcf433e19639699af57f727fba0
Eugene Darashkevich <evgenedar@mail.ru>
2024-05-28 19:57:42 +0300

    New three REST API routes (not tested)

--- src/services/users/app/main.py
+++ src/services/users/app/main.py
@@ -10,8 +10,12 @@ from kafka import KafkaProducer
 from database.database import db
 from util.util import get_field, password_hasher, connect_to_db, proto_post_to_dict
 from schemas.user_schema import UserSchema
+
 from api.posts.posts_pb2 import CreatePostRequest, UpdatePostRequest, DeletePostRequest, GetPostByIdRequest, ListPostsRequest
 from api.posts.posts_pb2_grpc import PostServiceStub
+
+from api.statistics.statistics_pb2 import ViewsAndLikesRequest, TopPostsRequest, Empty
+from api.statistics.statistics_pb2_grpc import StatisticsServiceStub
 
 # Flask
 app = Flask(__name__)
@@ -214,6 +218,64 @@ def add_post_like(post_id):
     }
     producer.send('post_likes', message)
     return jsonify({'success': True})
+
+
+@app.route("/posts/<post_id>/statistics", methods=["GET"])
+def get_post_statistics(post_id):
+    with grpc.insecure_channel('statistics-service:5100') as channel:
+        stub = StatisticsServiceStub(channel)
+        response = stub.GetTotalViewsAndLikes(ViewsAndLikesRequest(
+            post_id=post_id
+        ))
+    return jsonify({'post_id': post_id, 'views': response.views, 'likes': response.likes})
+
+
+@app.route("/posts/top", methods=["GET"])
+def get_top_posts():
+    sort_by_param = request.args.get('sort_by', 'likes').upper()
+    sort_by = TopPostsRequest.SortBy.LIKES if sort_by_param == 'LIKES' else TopPostsRequest.SortBy.VIEWS
+
+    with grpc.insecure_channel('statistics-service:5100') as channel:
+        stub = StatisticsServiceStub(channel)
+        response = stub.GetTopPosts(TopPostsRequest(
+            sort_by=sort_by
+        ))
+
+    if sort_by_param == 'LIKES':
+        posts = [
+            {
+                'post_id': p.post_id,
+                'author_username': User.query.filter_by(id=p.author_id).first().username,
+                'likes': p.likes
+            }
+            for p in response.posts
+        ]
+    else:
+        posts = [
+            {
+                'post_id': p.post_id,
+                'author_username': User.query.filter_by(id=p.author_id).first().username,
+                'views': p.views
+            }
+            for p in response.posts
+        ]
+    return jsonify(posts)
+
+
+@app.route("/users/top", methods=["GET"])
+def get_top_users():
+    with grpc.insecure_channel('statistics-service:5100') as channel:
+        stub = StatisticsServiceStub(channel)
+        response = stub.GetTopUsers(Empty())
+
+    users = [
+        {
+            'username': User.query.filter_by(id=u.user_id).first().username,
+            'likes': u.likes
+        }
+        for u in response.users
+    ]
+    return jsonify(users)
 
 
 if __name__ == "__main__":
