d4a6c4faa69d9cf22ab59f7cb1dfb1d15dcff41e
Eugene Darashkevich <evgenedar@mail.ru>
2024-05-31 01:16:21 +0300

    unit tests template

--- src/services/statistics/app/main.py
+++ src/services/statistics/app/main.py
@@ -18,36 +18,42 @@ from util.templates import (
 )
 
 
+def build_views_and_likes_response(response):
+    return ViewsAndLikesResponse(views=int(response[0][0]), likes=int(response[0][1]))
+
+
+def build_top_posts_responses(response, sorted_by):
+    return TopPostsResponse(posts=[
+        Post(post_id=row[0], **{sorted_by: int(row[1])})
+        for row in response
+    ])
+
+
+def build_top_users_responses(response):
+    return TopUsersResponse(users=[
+        User(user_id=row[0], likes=int(row[1]))
+        for row in response
+    ])
+
+
 class StatisticsService(StatisticsServiceServicer):
 
     def GetTotalViewsAndLikes(self, request, context):
         query = GET_TOTAL_VIEWS_AND_LIKES_TEMPLATE.format(post_id=request.post_id)
         result = clickhouse_request(query)
-        return ViewsAndLikesResponse(views=int(result[0][0]), likes=int(result[0][1]))
+        return build_views_and_likes_response(result)
 
     def GetTopPosts(self, request, context):
-        if request.sort_by == TopPostsRequest.LIKES:
-            query = GET_TOP_POSTS_TEMPLATE.format(table_name='post_likes')
-            result = clickhouse_request(query)
-            return TopPostsResponse(posts=[
-                Post(post_id=result[i][0], likes=int(result[i][1]))
-                for i in range(len(result))
-            ])
-        else:
-            query = GET_TOP_POSTS_TEMPLATE.format(table_name='post_views')
-            result = clickhouse_request(query)
-            return TopPostsResponse(posts=[
-                Post(post_id=result[i][0], views=int(result[i][1]))
-                for i in range(len(result))
-            ])
+        sorted_by = request.sort_by == TopPostsRequest.LIKES
+        table_name = 'post_{event}'.format(event=sorted_by)
+        query = GET_TOP_POSTS_TEMPLATE.format(table_name=table_name)
+        result = clickhouse_request(query)
+        return build_top_posts_responses(result, sorted_by)
 
     def GetTopUsers(self, request, context):
         query = GET_TOP_USERS_TEMPLATE.format(table_name='post_likes')
         result = clickhouse_request(query)
-        return TopUsersResponse(users=[
-            User(user_id=result[i][0], likes=int(result[i][1]))
-            for i in range(len(result))
-        ])
+        return build_top_users_responses(result)
 
 
 def serve():
