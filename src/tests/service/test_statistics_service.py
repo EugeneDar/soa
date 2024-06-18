7b7f3ea29f40a4e815895d40f118d3153d6cb9c3
Eugene Darashkevich <evgenedar@mail.ru>
2024-06-19 02:21:09 +0300

    build containers in yml

--- src/tests/service/test_statistics_service.py
+++ src/tests/service/test_statistics_service.py
@@ -10,15 +10,16 @@ class TestMyService(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        subprocess.run(['docker-compose', 'build', cls.path_to_docker], check=True)
-        subprocess.run(['docker-compose', 'up', '-d', cls.path_to_docker], check=True)
+        # subprocess.run(['docker-compose', 'build', cls.path_to_docker], check=True)
+        # subprocess.run(['docker-compose', 'up', '-d', cls.path_to_docker], check=True)
 
         time.sleep(10)  # время ожидания, чтобы контейнер заработал
 
     @classmethod
     def tearDownClass(cls):
         # Останавливает docker-compose
-        subprocess.run(['docker-compose', 'down'], check=True)
+        # subprocess.run(['docker-compose', 'down'], check=True)
+        pass
 
     def test_create_user(self):
         user_data = {
