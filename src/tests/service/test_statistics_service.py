044dcdceed566054cc3f7bee8a948e6787148352
Eugene Darashkevich <evgenedar@mail.ru>
2024-06-19 02:48:30 +0300

    some changes

--- src/tests/service/test_statistics_service.py
+++ src/tests/service/test_statistics_service.py
@@ -1,32 +1,43 @@
-import subprocess
 import time
-import requests
 import unittest
+import grpc
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from services.statistics.app.api.statistics.statistics_pb2 import ViewsAndLikesRequest, TopPostsRequest, Empty
+from services.statistics.app.api.statistics.statistics_pb2_grpc import StatisticsServiceStub
+
+
+POST_ID_1 = 'abacaba'
+LIKES_FOR_POST_1 = 2
+VIEWS_FOR_POST_1 = 5
+STATISTICS_SERVICE_ADDRESS = 'statistics-service:5100'
+
+
+def init_clickhouse():
+    pass
 
 
 class TestMyService(unittest.TestCase):
-    users_service_url = 'http://localhost:8000'
-    path_to_docker = './src/services'
 
     @classmethod
     def setUpClass(cls):
         time.sleep(10)
+        init_clickhouse()
 
     @classmethod
     def tearDownClass(cls):
         pass
 
-    def test_create_user(self):
-        user_data = {
-            "username": "testuser",
-            "password": "testpassword",
-            "phone": "8-800-53-53-535"
-        }
-        response = requests.post(f"{self.users_service_url}/signup", json=user_data)
-        self.assertEqual(response.status_code, 201)
-        self.assertIn("id", response.json())
-        # self.assertEqual(response.json()["phone"], user_data["phone"])
-        self.assertEqual(response.json()["phone"], "he-he")
+    def test_get_statistics_for_post(self):
+        with grpc.insecure_channel(STATISTICS_SERVICE_ADDRESS) as channel:
+            stub = StatisticsServiceStub(channel)
+            response = stub.GetTotalViewsAndLikes(ViewsAndLikesRequest(
+                post_id=POST_ID_1
+            ))
+        self.assertEqual(response.likes, LIKES_FOR_POST_1)
+        self.assertEqual(response.views, VIEWS_FOR_POST_1)
 
 
 if __name__ == '__main__':
